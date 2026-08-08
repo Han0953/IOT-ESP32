ddcc8b09990d1dcafe8a4b9f693c71bee576935a
Rehan <muhammadrehan302005@gmail.com>
2026-08-08 19:38:52 +0700

    Update IOT_Esp32.ino

--- IOT_Esp32.ino
+++ IOT_Esp32.ino
@@ -1,139 +1,148 @@
-\#include \<Servo.h>
-\#include \<EEPROM.h>
+/\*
 
-Servo baseServo;     // Servo 1: Pemutar lengan
-Servo gripperServo;  // Servo 2: Capit
-Servo extServo;      // Servo 3: Pemanjang lengan
-Servo liftServo;     // Servo 4: Pengangkat lengan
+- KODE LENGKAP IOT SMART HOME (Versi Final & Stabil)
+- Disesuaikan untuk Arduino IDE
+- Menggunakan metode non-blocking untuk animasi yang lebih stabil.
+  \*/
 
-const int pinBase    = 10;
-const int pinGripper = 9;
-const int pinExt     = 5;
-const int pinLift    = 7;
+\#include \<WiFi.h>
+\#include \<WebServer.h>
+\#include \<SPI.h>
+\#include \<Adafruit\_GFX.h>
+\#include \<Max72xxPanel.h>
 
-const int joyLeftX  = A0;
-const int joyLeftY  = A1;
-const int joyRightX = A2;
-const int joyRightY = A3;
+// --- KONFIGURASI JARINGAN ---
+const char\* ssid = "SmartHome";
+const char\* password = "";
 
-float angleBase, angleGripper, angleExt, angleLift;
+// --- KONFIGURASI PIN PERANGKAT ---
+const int PIN\_RELAY\_LAMPU = 27;
+const int PIN\_RELAY\_KIPAS = 26;
 
-unsigned long previousMillis = 0;
-const int interval = 15;
-unsigned long lastSaveTime = 0;
+// --- KONFIGURASI PIN DISPLAY MAX7219 ---
+const int pinCS = 5;
+const int numberOfHorizontalDisplays = 4;
+const int numberOfVerticalDisplays = 0;
 
+// Inisialisasi Web Server
+WebServer server(80);
+
+// Inisialisasi display
+Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+
+// --- Variabel untuk Animasi Non-Blocking ---
+String runningText = "SELAMAT DATANG DI SMART HOME"; // Teks awal berjalan dan jangan terbalik
+int scroll\_x; // Posisi x untuk scroll
+unsigned long lastScrollTime = 0;
+const int SCROLL\_SPEED = 50; // Kecepatan scroll dalam milidetik
+
+// --- FUNGSI-FUNGSI KONTROL (WEB HANDLER) ---
+
+void handleLampuON() {
+digitalWrite(PIN\_RELAY\_LAMPU, HIGH);
+server.send(200, "text/plain", "Lampu ON");
+}
+
+void handleLampuOFF() {
+digitalWrite(PIN\_RELAY\_LAMPU, LOW);
+server.send(200, "text/plain", "Lampu OFF");
+}
+
+void handleKipasON() {
+digitalWrite(PIN\_RELAY\_KIPAS, HIGH);
+server.send(200, "text/plain", "Kipas ON");
+}
+
+void handleKipasOFF() {
+digitalWrite(PIN\_RELAY\_KIPAS, LOW);
+server.send(200, "text/plain", "Kipas OFF");
+}
+
+void handleUbahText() {
+if (server.hasArg("pesan")) {
+runningText = server.arg("pesan");
+runningText.toUpperCase();
+scroll\_x = matrix.width(); // Reset posisi scroll saat teks diubah
+server.send(200, "text/plain", "Teks diubah menjadi: " + runningText);
+} else {
+server.send(400, "text/plain", "Error: Argumen 'pesan' tidak ditemukan");
+}
+}
+
+void handleRoot() {
+server.send(200, "text/plain", "Server ESP32 Aktif!");
+}
+
+// --- FUNGSI SETUP ---
 void setup() {
-Serial.begin(9600);
+Serial.begin(115200);
 
-// Baca memori lama biar gak nyentak balik ke 90 derajat
-angleBase    = EEPROM.read(0);
-angleGripper = EEPROM.read(1);
-angleExt     = EEPROM.read(2);
-angleLift    = EEPROM.read(3);
+pinMode(PIN\_RELAY\_LAMPU, OUTPUT);
+pinMode(PIN\_RELAY\_KIPAS, OUTPUT);
+digitalWrite(PIN\_RELAY\_LAMPU, LOW);
+digitalWrite(PIN\_RELAY\_KIPAS, LOW);
 
-// Proteksi kalau data EEPROM corrupt
-if (angleBase > 180 || angleBase < 0) angleBase = 90;
-if (angleGripper > 180 || angleGripper < 0) angleGripper = 90;
-if (angleExt > 180 || angleExt < 0) angleExt = 90;
-if (angleLift > 180 || angleLift < 0) angleLift = 90;
+// Setup display
+matrix.setIntensity(7);
+matrix.setRotation(4); // Nilai rotasi terakhir yang kita coba
+scroll\_x = matrix.width(); // Atur posisi awal scroll
 
-// Set posisi sebelum di-attach
-baseServo.write((int)angleBase);
-gripperServo.write((int)angleGripper);
-extServo.write((int)angleExt);
-liftServo.write((int)angleLift);
+// Membuat Access Point (AP)
+Serial.print("\nMembuat Access Point: ");
+Serial.println(ssid);
+WiFi.softAP(ssid, password);
+
+IPAddress myIP = WiFi.softAPIP();
+Serial.print("Alamat IP AP: ");
+Serial.println(myIP);
+
+// Daftarkan URL endpoint
+server.on("/", handleRoot);
+server.on("/lampu\_on", handleLampuON);
+server.on("/lampu\_off", handleLampuOFF);
+server.on("/kipas\_on", handleKipasON);
+server.on("/kipas\_off", handleKipasOFF);
+server.on("/ubah\_text", handleUbahText);
+
+server.begin();
+Serial.println("HTTP server dimulai");
 }
 
+// --- FUNGSI LOOP (Struktur Non-Blocking yang Benar) ---
 void loop() {
-unsigned long currentMillis = millis();
+// Selalu tangani permintaan dari client. Ini akan berjalan sangat cepat dan responsif.
+server.handleClient();
 
-if (currentMillis - previousMillis >= interval) {
-previousMillis = currentMillis;
+// Cek apakah sudah waktunya untuk menggeser teks
+if (millis() - lastScrollTime > SCROLL\_SPEED) {
+lastScrollTime = millis(); // Catat waktu terakhir scroll
 
 ```
-int valLeftX  = analogRead(joyLeftX);
-int valLeftY  = analogRead(joyLeftY);
-int valRightX = analogRead(joyRightX);
-int valRightY = analogRead(joyRightY);
+// Hapus layar
+matrix.fillScreen(LOW);
 
-// Deteksi pergerakan tuas (di luar deadzone 480-540)
-bool isBaseMoving    = (valLeftX < 480 || valLeftX > 540);  // Kiri X
-bool isLiftMoving    = (valLeftY < 480 || valLeftY > 540);  // Kiri Y (Naik-Turun)
-bool isExtMoving     = (valRightX < 480 || valRightX > 540); // Kanan X (Maju-Mundur)
-bool isGripperMoving = (valRightY < 480 || valRightY > 540); // Kanan Y (Capit)
+// Atur posisi kursor
+matrix.setCursor(scroll_x, 0);
 
-// =======================================================
-// BAGIAN KONTROL JOYSTICK (SUDAH DIUPDATE)
-// =======================================================
+// Cetak string
+matrix.print(runningText);
 
-// ---- KONTROL JOYSTICK KIRI ----
-if (isBaseMoving) {
-  aktifkanSatuServo(1); // Nyalakan Base aja
-  updateServoAngle(valLeftX, angleBase, baseServo);
-} 
-else if (isLiftMoving) {
-  aktifkanSatuServo(4); // Nyalakan Lift (Naik/Turun) aja
-  updateServoAngle(valLeftY, angleLift, liftServo);
-} 
+// Tampilkan ke display
+matrix.write();
 
-// ---- KONTROL JOYSTICK KANAN ----
-else if (isExtMoving) {
-  aktifkanSatuServo(3); // Nyalakan Ext (Maju/Mundur Lengan) aja
-  updateServoAngle(valRightX, angleExt, extServo);
-} 
-else if (isGripperMoving) {
-  aktifkanSatuServo(2); // Nyalakan Capit aja
-  updateServoAngle(valRightY, angleGripper, gripperServo);
-} 
+// Geser posisi untuk frame berikutnya
+scroll_x--;
 
-// ---- KONDISI DIAM (JOYSTICK DI TENGAH) ----
-else {
-  aktifkanSatuServo(0); // Matikan arus ke semua servo
-
-  // Simpan memori posisi terakhir setelah diam 1 detik
-  if (millis() - lastSaveTime > 1000) {
-    EEPROM.update(0, (int)angleBase);
-    EEPROM.update(1, (int)angleGripper);
-    EEPROM.update(2, (int)angleExt);
-    EEPROM.update(3, (int)angleLift);
-    lastSaveTime = millis();
-  }
+// Jika teks sudah hilang dari layar, reset posisinya ke kanan
+// 6 adalah lebar karakter (5) + spasi (1)
+if (scroll_x > -(runningText.length() * 6)) {
+  scroll_x = matrix.width();
 }
 ```
 
 }
 }
 
-// Fungsi hemat listrik (hanya 1 servo nyala)
-void aktifkanSatuServo(int activeId) {
-if (activeId == 1) { if (!baseServo.attached()) baseServo.attach(pinBase); }
-else { if (baseServo.attached()) baseServo.detach(); }
-
-if (activeId == 2) { if (!gripperServo.attached()) gripperServo.attach(pinGripper); }
-else { if (gripperServo.attached()) gripperServo.detach(); }
-
-if (activeId == 3) { if (!extServo.attached()) extServo.attach(pinExt); }
-else { if (extServo.attached()) extServo.detach(); }
-
-if (activeId == 4) { if (!liftServo.attached()) liftServo.attach(pinLift); }
-else { if (liftServo.attached()) liftServo.detach(); }
-}
-
-// Fungsi penggerak halus (smoothing)
-void updateServoAngle(int joyVal, float &currentAngle, Servo &targetServo) {
-float stepSpeed = 0.0;
-if (joyVal < 480) {
-stepSpeed = map(joyVal, 479, 0, 1, 10) / 10.0;
-currentAngle -= stepSpeed;
-}
-else if (joyVal > 540) {
-stepSpeed = map(joyVal, 541, 1023, 1, 10) / 10.0;
-currentAngle += stepSpeed;
-}
-currentAngle = constrain(currentAngle, 0, 180);
-targetServo.write((int)currentAngle);
-}
 
 
-
-Dari codingan ini kamu buatkan readmenya, ini arm robot
+Ini untuk codenya, update lagi readmenya dan disesuaikan
